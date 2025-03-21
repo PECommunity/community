5acd7457b3ec050fbb8e58a4788d420d0f1f38d6
Chris (Gentle) Y杨 <nodexy@gmail.com>
2025-03-21 18:03:29 +0800

    Update theme.config.tsx

--- site/theme.config.tsx
+++ site/theme.config.tsx
@@ -5,7 +5,7 @@ import { useConfig } from 'nextra-theme-docs'
 import Footer from './components/Footer'
 
 const config: DocsThemeConfig = {
-  logo: '<span style="display:flex;"><img src="/icon.svg" alt="PECommunity"/> <span style="font-size: 24px;line-height:48px;padding-left:5px;">平台工程社区</span></span>',
+  logo: <span style="display:flex;"><img src="/icon.svg" alt="PECommunity"/><span style="font-size: 24px;line-height:48px;padding-left:5px;">平台工程社区</span></span>,
   project: {
     link: 'https://github.com/PECommunity/community'
   },
