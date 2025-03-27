e7c6d1e6f7da17c5b36522e8e324cf6d4f7b7421
Chris (Gentle) Y杨 <nodexy@gmail.com>
2025-03-27 18:10:34 +0800

    Update and rename next.config.js to next.config.mjs

---
+++ site/next.config.mjs
@@ -0,0 +1,13 @@
+import withNextra from 'nextra';
+
+const nextraConfig = withNextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+});
+
+export default nextraConfig({
+  webpack: (config, { buildId, dev }) => {
+    config.resolve.symlinks = false;
+    return config;
+  },
+});
