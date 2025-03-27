e3d378da06ed3edb3551b9899b794555da2cefa3
Chris (Gentle) Y杨 <nodexy@gmail.com>
2025-03-27 17:59:58 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -1,17 +1,1 @@
-async function nextConfig() {
-  const { default: withNextra } = await import('nextra');
 
-  const nextraConfig = withNextra({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.tsx',
-  });
-
-  return nextraConfig({
-    webpack: (config, { buildId, dev }) => {
-      config.resolve.symlinks = false;
-      return config;
-    },
-  });
-}
-
-module.exports = nextConfig();
