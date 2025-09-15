ef09232f4b0a8064be27952a2e6dc209af7598f5
Chris (Gentle) Y杨 <nodexy@gmail.com>
2025-09-15 15:09:50 +0800

    improve next confif

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,9 +1,18 @@
-import withNextra from 'nextra';
+//import withNextra from 'nextra';
+//
+//const nextraConfig = withNextra({
+//  theme: 'nextra-theme-docs',
+//  themeConfig: './theme.config.tsx',
+//});
 
-const nextraConfig = withNextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-});
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',      
+  themeConfig: './theme.config.jsx' 
+})
+
+module.exports = withNextra({
+  reactStrictMode: true,
+})
 
 export default nextraConfig({
   webpack: (config, { buildId, dev }) => {
